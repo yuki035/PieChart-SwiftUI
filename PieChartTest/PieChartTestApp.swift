6bef45af17e87d258b6a32fe4c630a4b6e62ff5c
yuki035 <yukiyuki1918@icloud.com>
2022-10-21 09:34:09 +0900

    Initial Commit

---
+++ PieChartTest/PieChartTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartTestApp.swift
+//  PieChartTest
+//
+//  Created by 森祐樹 on 2022/10/21.
+//
+
+import SwiftUI
+
+@main
+struct PieChartTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
