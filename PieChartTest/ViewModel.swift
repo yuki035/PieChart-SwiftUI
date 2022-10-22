f852ee352e629fd90a6d63b5d8142be2ca0c2dea
yuki035 <yukiyuki1918@icloud.com>
2022-10-22 20:56:19 +0900

    clean: リファクタリング

--- PieChartTest/ViewModel.swift
+++ PieChartTest/ViewModel.swift
@@ -11,28 +11,26 @@ class PieChartViewModel: ObservableObject {
     @Published var PieChartDatas: [PieChartModel] = []
     
     init(sumArray: [Int]) {
-        let fromToArray = calcFromTo(sumArray: sumArray)
+        setup(sumArray: sumArray)
+    }
+    
+    func setup(sumArray: [Int]) {
+        let ratioArray = calcRatioArray(sumArray: sumArray)
+        let fromToArray = calcFromTo(ratioArray: ratioArray)
         setupPieChartDatas(fromToArray: fromToArray)
     }
     
-    func setupPieChartDatas(fromToArray: [(from: CGFloat, to: CGFloat)]) {
-        var i = 0
-        for batArea in PieChartModel.BatAreas.allCases {
-            PieChartDatas.append(PieChartModel(batArea: batArea,
-                                               from: fromToArray[i].from,
-                                               to: fromToArray[i].to))
-            i+=1
-        }
-    }
+}
+// MARK: Functions for setup
+private extension PieChartViewModel {
     
-    // 引数：[P.sum, C.sum, I.sum, Out.sum, Other.sum]
-    // 円グラフの描画に必要な(from, to)の配列を返す
-    func calcFromTo(sumArray: [Int]) -> [(from: CGFloat, to: CGFloat)]{
-        
+    /// 要素の合計値に対する各要素の割合を配列として返す
+    /// - Parameter sumArray: BatAreaに打った合計値をそれぞれ保持した配列
+    /// - Returns: 各要素の割合を保持した配列
+    func calcRatioArray(sumArray: [Int]) -> [CGFloat] {
         let sum: Int = sumArray.reduce(0, +)
         var s: [Int] = [0, 0, 0, 0 ,0 ,0]
         var ratioArray: [CGFloat] = []
-        var ret: [(from: CGFloat, to: CGFloat)] = []
         
         // 累積和を計算
         for i in 0..<sumArray.count {
@@ -41,11 +39,33 @@ class PieChartViewModel: ObservableObject {
         
         ratioArray = s.map { CGFloat($0) / CGFloat(sum) }
         
-        for i in 0..<s.count-1 {
-            ret.append((from: ratioArray[i], to: ratioArray[i+1]))
+        return ratioArray
+    }
+    
+    /// 円グラフの描画に必要な(from, to)の配列を返す
+    /// - Parameter ratioArray:　要素の合計値に対する各要素の割合を保持する配列
+    /// - Returns: 円グラフの描画に必要なタプルの配列
+    func calcFromTo(ratioArray: [CGFloat]) -> [(from: CGFloat, to: CGFloat)]{
+        
+        var fromToArray: [(from: CGFloat, to: CGFloat)] = []
+        
+        for i in 0..<ratioArray.count-1 {
+            fromToArray.append((from: ratioArray[i], to: ratioArray[i+1]))
         }
         
-        return ret
+        return fromToArray
+    }
+    
+    /// PieChartDatasに値を与える
+    /// - Parameter fromToArray: 円グラフの描画に必要なタプルの配列
+    func setupPieChartDatas(fromToArray: [(from: CGFloat, to: CGFloat)]) {
+        var i = 0
+        for batArea in PieChartModel.BatAreas.allCases {
+            PieChartDatas.append(PieChartModel(batArea: batArea,
+                                               from: fromToArray[i].from,
+                                               to: fromToArray[i].to))
+            i+=1
+        }
     }
     
 }
