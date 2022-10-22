a3fde75ab8e9859819882981e8cde5ab6f609702
yuki035 <yukiyuki1918@icloud.com>
2022-10-22 20:15:46 +0900

    clean: 円グラフ関連のViewをまとめた

--- PieChartTest/ContentView.swift
+++ PieChartTest/ContentView.swift
@@ -12,32 +12,7 @@ struct ContentView: View {
     var body: some View {
         ScrollView {
             Spacer().frame(height: 300)
-            HStack(spacing: 40) {
-                PieChartView()
-                    .padding()
-                HStack(){
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("■　").foregroundColor(.red)+Text("投手")
-                        Text("■　").foregroundColor(.blue)+Text("捕手")
-                        Text("■　").foregroundColor(.yellow)+Text("内野")
-                        Text("■　").foregroundColor(.green)+Text("外野")
-                        Text("■　").foregroundColor(.gray)+Text("その他")
-                    }
-                    VStack(spacing: 5) {
-                        Text("14.3%")
-                        Text("14.3%")
-                        Text("14.3%")
-                        Text("14.3%")
-                        Text("14.3%")
-                    }
-                }
-                .font(.footnote)
-
-            }
-            .padding()
-            .frame(width: 350)
-            .background()
-            .cornerRadius(15)
+            PieChartWholeView()
         }
         .frame(width: 466.0)
         .background(normalBackgroundColor)
