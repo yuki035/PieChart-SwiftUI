d05f3ef7e4c13ee7ae213e55c723e62cb4e40c2f
yuki035 <yukiyuki1918@icloud.com>
2022-10-22 20:14:37 +0900

    add: ファイルを追加

---
+++ PieChartTest/PieChartWholeView.swift
@@ -0,0 +1,20 @@
+//
+//  PieChartWholeView.swift
+//  PieChartTest
+//
+//  Created by 森祐樹 on 2022/10/22.
+//
+
+import SwiftUI
+
+struct PieChartWholeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PieChartWholeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PieChartWholeView()
+    }
+}
